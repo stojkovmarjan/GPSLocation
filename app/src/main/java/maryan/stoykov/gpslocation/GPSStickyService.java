14210a2f1c4fed436a1861f2fe0dda6ca13fdfb3
stojkovmarjan <stojkovmarjan@gmail.com>
2023-08-06 08:11:20 +0200

    Initial Commit

---
+++ app/src/main/java/maryan/stoykov/gpslocation/GPSStickyService.java
@@ -0,0 +1,61 @@
+package maryan.stoykov.gpslocation;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class GPSStickyService extends Service {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            Log.e ("Service", "Service is running");
+                            try {
+                                Thread.sleep(2000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+        ).start();
+
+        startForeground(1001, SetNotification().build());
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private Notification.Builder SetNotification () {
+        final String CHANNEL_ID = "My Foreground ID";
+
+        NotificationChannel notificationChannel = new NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_ID,
+                NotificationManager.IMPORTANCE_LOW
+        );
+
+        getSystemService(NotificationManager.class).createNotificationChannel(notificationChannel);
+
+        Notification.Builder notification = new Notification.Builder (this, CHANNEL_ID)
+                .setContentText("Foreground service is running!")
+                .setContentTitle("Service enabled")
+                .setSmallIcon(R.drawable.ic_launcher_background);
+
+        return notification;
+    }
+}
---
+++ app/src/main/java/maryan/stoykov/gpslocation/MainActivity.java
@@ -0,0 +1,19 @@
+package maryan.stoykov.gpslocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent serviceIntent = new Intent(this, GPSStickyService.class);
+
+        startForegroundService(serviceIntent);
+    }
+}
