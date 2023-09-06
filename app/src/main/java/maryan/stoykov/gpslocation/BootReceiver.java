2eb7c6176409452e3603d7cd3ee77df8986abedb
stojkovmarjan <stojkovmarjan@gmail.com>
2023-09-06 23:10:13 +0200

    UnsafeProtectedBroadcastReceiver

--- app/src/main/java/maryan/stoykov/gpslocation/BootReceiver.java
+++ app/src/main/java/maryan/stoykov/gpslocation/BootReceiver.java
@@ -1,10 +1,12 @@
 package maryan.stoykov.gpslocation;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 public class BootReceiver extends BroadcastReceiver {
+    @SuppressLint("UnsafeProtectedBroadcastReceiver")
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent serviceIntent = new Intent(context, GPSStickyService.class);
