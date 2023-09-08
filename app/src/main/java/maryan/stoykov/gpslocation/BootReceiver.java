5b974e72fd46c7b85b4fc6247f5938765ecd5e69
stojkovmarjan <stojkovmarjan@gmail.com>
2023-09-08 20:12:39 +0200

    restart and power off

--- app/src/main/java/maryan/stoykov/gpslocation/BootReceiver.java
+++ app/src/main/java/maryan/stoykov/gpslocation/BootReceiver.java
@@ -10,6 +10,15 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Intent serviceIntent = new Intent(context, GPSStickyService.class);
+        if (intent.getAction() != null) {
+            if (intent.getAction().equals(Intent.ACTION_SHUTDOWN)) {
+                // Handle power-off event
+                // This code will execute when the device is shutting down.
+            } else if (intent.getAction().equals(Intent.ACTION_REBOOT)) {
+                // Handle reboot event
+                // This code will execute when the device is restarting.
+            }
+        }
         context.startForegroundService(serviceIntent);
     }
 }
