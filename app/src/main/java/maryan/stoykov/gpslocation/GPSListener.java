2f0de61ed304f2c60dfb96fc9534973430f71890
stojkovmarjan <stojkovmarjan@gmail.com>
2023-09-10 15:03:04 +0200

    added all providers with SDK condition

--- app/src/main/java/maryan/stoykov/gpslocation/GPSListener.java
+++ app/src/main/java/maryan/stoykov/gpslocation/GPSListener.java
@@ -1,9 +1,11 @@
 package maryan.stoykov.gpslocation;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 
@@ -19,11 +21,13 @@ import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
 
+import java.util.List;
+
 /**
  * provides the main service with location data
  * taken from the FUSED LOCATION provider
  */
-public class GPSListener  {
+public class GPSListener implements LocationListener {
     private final String className = this.getClass().getSimpleName();
     private final Context context;
     private LocationManager locationManager;
@@ -38,7 +42,7 @@ public class GPSListener  {
         this.location = location;
     }
 
-    GPSListenerOnChange gpsListenerOnChange; // event listener ( for sticky service only for now)
+    private final GPSListenerOnChange gpsListenerOnChange; // event listener ( for sticky service only for now)
     private final FusedLocationProviderClient fusedLocationClient;
     private final LocationCallback locationCallback  = new LocationCallback() {
         @Override
@@ -62,10 +66,9 @@ public class GPSListener  {
 
     protected void requestLocation() {
 
-            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED
-                    && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
                 //    ActivityCompat#requestPermissions
                 // here to request the missing permissions, and then overriding
@@ -74,25 +77,65 @@ public class GPSListener  {
                 // to handle the case where the user grants the permission. See the documentation
                 // for ActivityCompat#requestPermissions for more details.
                 return;
-            }
-
-//            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-//                updateTime, updateDistance, this);
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
-                LocationRequest locationRequest = new LocationRequest.Builder(updateTime)
-                        .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
-                        .setMinUpdateIntervalMillis(minUpdateTime)
-                        .setMinUpdateDistanceMeters(minUpdateDistance)
-                        .setGranularity(Granularity.GRANULARITY_FINE)
-                        .build();
-
-                fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
-            }
-
         }
-    public void stopLocationUpdate(){
-        fusedLocationClient.removeLocationUpdates(locationCallback);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            startUsingFusedProvider();
+        } else {
+            startUsingNetworkProvider();
+            startUsingGpsProvider();
+        }
+
     }
 
+
+    // permission is checked in requestLocation() method before those 3 methods are called
+    @SuppressLint("MissingPermission")
+    private void startUsingNetworkProvider(){
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, updateTime, minUpdateDistance, this);
+    }
+    @SuppressLint("MissingPermission")
+    private void startUsingGpsProvider(){
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, updateTime, minUpdateDistance, this);
+    }
+
+    @SuppressLint("MissingPermission")
+    public void startUsingFusedProvider(){
+        LocationRequest locationRequest = new LocationRequest.Builder(updateTime)
+                .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+                .setMinUpdateIntervalMillis(minUpdateTime)
+                .setMinUpdateDistanceMeters(minUpdateDistance)
+                .setGranularity(Granularity.GRANULARITY_FINE)
+                .build();
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+    }
+    public void stopLocationUpdate(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            fusedLocationClient.removeLocationUpdates(locationCallback);
+        } else {
+            locationManager.removeUpdates(this);
+        }
+    }
 }
