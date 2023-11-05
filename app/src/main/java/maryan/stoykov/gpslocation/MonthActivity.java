7ea1dcbe4a4c6ea2290529acd933b804bb5ad4f1
stojkovmarjan <stojkovmarjan@gmail.com>
2023-11-05 18:12:09 +0100

    added ReportActivity for month

--- app/src/main/java/maryan/stoykov/gpslocation/MonthActivity.java
+++ app/src/main/java/maryan/stoykov/gpslocation/MonthActivity.java
@@ -3,6 +3,7 @@ package maryan.stoykov.gpslocation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -10,12 +11,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Calendar;
+import java.util.Date;
 
 public class MonthActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private String[] months = new String[]{"Select Month","January","February","March","April","May",
+    private final String[] months = new String[]{"Select Month","January","February","March","April","May",
             "June","July","August","September","October","November","December"};
 
     @Override
@@ -35,7 +40,7 @@ public class MonthActivity extends AppCompatActivity implements AdapterView.OnIt
         Spinner spMonth = findViewById(R.id.spMonth);
         spMonth.setOnItemSelectedListener(this);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this, R.layout.spinner_item, months);
 
         arrayAdapter.setDropDownViewResource(R.layout.spinner_item);
@@ -51,10 +56,22 @@ public class MonthActivity extends AppCompatActivity implements AdapterView.OnIt
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+        if (position>0 && position<= getCurrentMonthIndex()){
+            Intent intent = new Intent(this, ReportActivity.class);
+            intent.putExtra("Month",position);
+            startActivity(intent);
+        }
+
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    private int getCurrentMonthIndex(){
+        Date date = new Date();
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return localDate.getMonthValue();
     }
 }
